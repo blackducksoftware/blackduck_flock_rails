8232745bf3c2de35234047f6be00fe0fe979aaa6
Daniel Rubio <drubio1989@gmail.com>
2017-01-14 19:38:48 -0500

    Add employee index, new, and create test

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,21 @@
 class EmployeesController < ApplicationController
   def index
+    @employees = Employee.all
+  end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+    @employee.save
+    redirect_to employees_path 
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :title)
   end
 end
--- spec/controllers/employees_controller_spec.rb
+++ spec/controllers/employees_controller_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe EmployeesController, type: :controller do
       expect(response).to be_success
       expect(response).to render_template('index')
     end
+
+    it 'should show employees on the index page' do
+      employees = FactoryGirl.create_list(:employee, 2)
+      get :index
+      expect(assigns(:employees)).to eq(employees)
+    end
+   
+    it 'should create a new valid employee' do
+      employee_params = FactoryGirl.attributes_for(:employee)
+      expect { post :create, employee: employee_params }.to change(Employee, :count).by(1)
+      should redirect_to(action: :index)
+    end
   end
 end
