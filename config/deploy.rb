d02e6aac975156acfc21a04ddbcd4228b0d4e1dc
Daniel Rubio <drubio1989@gmail.com>
2017-05-07 18:10:31 -0400

    Explicitly set ruby env to /Users/drubio/.rvm/gems/ruby-2.2.5/bin:/Users/drubio/.rvm/gems/ruby-2.2.5@global/bin:/Users/drubio/.rvm/rubies/ruby-2.2.5/bin:/Users/drubio/.rvm/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin on remote

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,6 @@ set :deploy_to, "/var/local/blackduck_flock_rails"
 set :bundle_gemfile, -> { release_path.join('Gemfile') }      # default: nil
 set :bundle_path, -> { shared_path.join('') }
 set :bundle_flags, '--verbose'
-set :use_sudo, false
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -32,7 +31,7 @@ set :use_sudo, false
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
