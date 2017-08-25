570dd08b675daba8ae4d59bc746f8c2734f44b88
Daniel Rubio <drubio1989@gmail.com>
2017-08-25 10:03:09 -0400

    Remove admin after_action for employees

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -1,6 +1,5 @@
 module Admin
   class EmployeesController < Admin::ApplicationController
-    after_action :create_last_name, only: [:create]
     # To customize the behavior of this controller,
     # simply overwrite any of the RESTful actions. For example:
     #
@@ -16,11 +15,5 @@ module Admin
 
     # See https://administrate-docs.herokuapp.com/customizing_controller_actions
     # for more information
-    private
-
-    def create_last_name
-      employee = Employee.find_by_email(params[:employee][:email])
-      employee.update(last_name: employee.name.split.last)
-    end
   end
 end
